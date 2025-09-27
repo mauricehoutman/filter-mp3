491a7173d652438aab8fcd5f746344ec7ca90874
mauricehoutman <mauricehoutman@gmail.com>
2025-09-27 17:46:05 +0200

    changed path folder

--- filtermp3.py
+++ filtermp3.py
@@ -1,10 +1,10 @@
 import os
 import glob
 
-folder = "/raid5/music/Maurice" # Change folder path
+folder = "/path/to/folder" # Change folder path
 files = glob.glob(os.path.join(folder, "**", "*"), recursive=True)
 
 for file_path in files:
     if os.path.isfile(file_path):
-        if not file_path.lower().endswith("mp3"):
+        if not file_path.lower().endswith("mp3"): # Change file type
             os.remove(file_path)
